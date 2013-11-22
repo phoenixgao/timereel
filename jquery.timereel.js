dfae49c6b05533616cd3c13d1fe1916b0b3eda31
Xiang Gao <phoenix.x.gao@gmail.com>
2013-11-22 16:59:26 +0800

    first commit

---
+++ jquery.timereel.js
@@ -0,0 +1,15 @@
+/**
+ * Timereel v0.1.0 - 2013-11-22
+ * jQuery timeline plugin
+ * (c) 2013 Xiang Gao - https://github.com/phoenixgao/timereel
+ * license: http://www.opensource.org/licenses/mit-license.php
+ */
+(function($) {
+	$.fn.timereel = function(options) {
+		
+	};
+	
+	function init() {
+		
+	}
+})(jQuery);
